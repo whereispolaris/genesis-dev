86c3e4d108431734d666eb18ebc624f676114924
Santiago Enciso <whereispolaris@gmail.com>
2019-11-30 12:56:39 -0600

    Added action that checks if sidebar is active and displays front_page_1 widget with a div.

--- front-page.php
+++ front-page.php
@@ -1,6 +1,25 @@
 <?php
 
+add_action( 'genesis_meta', 'my_homepage_setup' );
+// Add widget support for homepage
+function my_homepage_setup() {
+
+	if ( is_active_sidebar( 'front-page-1' ) )  {
+		 // Add front page 1 widget
+		add_action( 'genesis_after_header', 'display_front_page_1_widget' );
+
+	}
+}
+
+// Display front page widgets.
+function display_front_page_1_widget() {
+
+	genesis_widget_area( 'front-page-1', array(
+		'before' => '<div class="front-page-1-class"><div class="wrap">',
+		'after'  => '</div></div>',
+	) );
+}
+
 // Load the genesis framework
 genesis();
-
 
